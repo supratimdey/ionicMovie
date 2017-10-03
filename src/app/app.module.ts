d3f90b83c4d2f50b55ffc335ab089a62100f35d1
Supratim <supratimdey@outlook.com>
2017-10-03 21:13:30 +0530

    upload source

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,12 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
+import { HttpModule } from '@angular/http';
+
+
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { RemoteServiceProvider } from '../providers/remote-service/remote-service';
 
 @NgModule({
   declarations: [
@@ -14,7 +18,8 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    HttpModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -24,7 +29,8 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    RemoteServiceProvider
   ]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,45 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { RemoteServiceProvider } from '..//../providers/remote-service/remote-service';
+
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
+  providers: [RemoteServiceProvider]
 })
-export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+export class HomePage {
+  
+  movies: Array<any>;
+
+  constructor(public navCtrl: NavController , private movieService: RemoteServiceProvider) {
 
   }
 
+  searchMovieDB(event,key) {
+    if(event.target.value.length > 2) {
+      this.movieService.searchMovies(event.target.value).subscribe(
+          data => {
+              this.movies = data.results; 
+              console.log(data);
+          },
+          err => {
+              console.log(err);
+          },
+          () => console.log('Movie Search Complete')
+      );
+   }
+  }
+
+  itemTapped(event, movie) {
+    console.log(movie);
+    alert(movie.title)  ;
+    //this.navController.push(MovieInfo, {
+      //  movie: movie
+   // });
+  }
+
 }
+
+
